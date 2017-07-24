4b18551426da2df59b7990d55a955f1e7ba47af9
e_ruchlewicz <ewelina.ruchlewicz@gmail.com>
2017-07-24 20:23:32 +0200

    Songbook spinner in add song view

--- app/src/main/java/com/example/ewelina/spisek/AddFragment.java
+++ app/src/main/java/com/example/ewelina/spisek/AddFragment.java
@@ -5,16 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class AddFragment extends Fragment {
+public class AddFragment extends Fragment implements AdapterView.OnItemSelectedListener{
 
     DatabaseHelper myDB;
-    EditText editTitle, editPlace, editPage, editNr, editLyrics;
+    EditText editTitle, editPage, editNr, editLyrics;
     Button btnDodaj;
-    Button btnWyswietl;
+    Spinner spinner2;
 
     public AddFragment() {
     }
@@ -23,12 +26,16 @@ public class AddFragment extends Fragment {
         myDB = new DatabaseHelper(getActivity());
         View v = inflater.inflate(R.layout.fragment_add, container, false);
         editTitle = (EditText)v.findViewById(R.id.editText_title);
-        editPlace = (EditText)v.findViewById(R.id.editText_place);
+        spinner2 = (Spinner)v.findViewById(R.id.spinner2);
         editPage = (EditText)v.findViewById(R.id.editText_page);
         editNr = (EditText)v.findViewById(R.id.editText_nr);
         editLyrics = (EditText)v.findViewById(R.id.editText_lyrics);
         btnDodaj = (Button)v.findViewById(R.id.button_add);
         Dodaj();
+        spinner2 = (Spinner) v.findViewById(R.id.spinner2);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.songbooks, android.R.layout.simple_spinner_item);
+        spinner2.setAdapter(adapter);
+        spinner2.setOnItemSelectedListener(this);
         return v;
     }
 
@@ -41,20 +48,29 @@ public class AddFragment extends Fragment {
                     editPage.setError( "Podaj numer strony" );
                 if( editTitle.getText().toString().length() == 0 )
                     editTitle.setError( "Tytuł jest wymagany!" );
-                if( editPlace.getText().toString().length() == 0 )
-                    editPlace.setError( "Wybierz śpiewnik" );
+                if( spinner2.getSelectedItem().toString().length() == 0 )
+                    Toast.makeText(getActivity(), "Wybierz śpiewnik!", Toast.LENGTH_LONG).show();
                 if( editPage.getText().toString().length() == 0 )
                     editPage.setError( "Podaj stronę" );
 
-                if( editPage.getText().toString().length() > 0 && editPlace.getText().toString().length() > 0
+                if( editPage.getText().toString().length() > 0 && spinner2.getSelectedItem().toString().length() > 0
                         && editPage.getText().toString().length() > 0 && editPage.getText().toString().trim().matches(regexStr))
                 {
 
-                boolean isInserted = myDB.insertData(editTitle.getText().toString(), editPlace.getText().toString(), editPage.getText().toString(),
+                boolean isInserted = myDB.insertData(editTitle.getText().toString(), spinner2.getSelectedItem().toString(), editPage.getText().toString(),
                         editNr.getText().toString(), editLyrics.getText().toString());
                 if(isInserted == true) Toast.makeText(getActivity(), "Piosenka została dodana.", Toast.LENGTH_LONG).show();
                 else Toast.makeText(getActivity(), "Piosenka nie została dodana.", Toast.LENGTH_LONG).show();}
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
